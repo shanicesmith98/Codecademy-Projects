6568849949ae82d8dafc722e377f6df0b0129a1d
shanice <shanicesmith.work@hotmail.com>
2019-10-11 18:54:16 -0400

    Update temperature.js

--- Javascript/temperature.js
+++ Javascript/temperature.js
@@ -1,13 +1,13 @@
 // current forecast in kelvin
 var kelvin = prompt("What is the temperature today in Kelvin?");
-// celsius formula conversion (value can't be changed)
-let celsius = kelvin - 273;
+// celsius formula conversion
+var celsius = kelvin - 273;
 // newton scale formula
-var newton = celsius * (33/100);
+let newton = celsius * (33/100);
 // round down value of newton
 newton = Math.floor(newton);
 // fahrenheit formula conversion
-var fahrenheit = celsius * (9/5) + 32;
+let fahrenheit = celsius * (9/5) + 32;
 // round down value of fahrenheit
 fahrenheit = Math.floor(fahrenheit);
 
